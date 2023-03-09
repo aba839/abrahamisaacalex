01ac184d035e4332c794cf2d074ef1abbae14d3c
Antonio Jimenez <antonioisaacjimenez12@gmail.com>
2023-03-09 13:39:02 +0100

    modificacion

--- Ejercicio.php
+++ Ejercicio.php
@@ -7,41 +7,53 @@ $emailconfirmacion = "pepehernandez@gmail.com";
 $dni = "72184616G";
 $contraseña = "arbusto";
 
-/*for ( $pos=0; $pos < strlen($nombre); $pos ++ ) {
+for ( $pos=0; $pos < strlen($nombre); $pos ++ ) {
     $byte = substr($nombre, $pos); 
     echo "El byte " . $pos . " de $nombre tiene el valor " . ord($byte) . "<br>" ;
-   }
+}
+
 function validarMayus($nombre,$apellido){
-if(ord($nombre[0])>=65 and ord($nombre[0])<=90){
-    return true;
-    else {
-    return false;
+    if(ord($nombre[0])>=65 and ord($nombre[0])<=90){
+        return true;
+    } else {
+        return false;
     }
 }
+
+if(validarMayus($nombre, $apellido)){
+    echo "La primera letra del nombre es una mayúscula.<br>";
+} else {
+    echo "La primera letra del nombre no es una mayúscula.<br>";
 }
+
 function validarLetra($nombre,$apellido){
-    for ( $pos=0; $pos < strlen($nombre); $pos ++ ) {
-        $byte = substr($nombre, $pos);
-        if(ord($byte)>=97 and ord($byte)<=122)
-        echo "correcto";
-        else 
-        echo "incorrecto";
-}
+    if(validarMayus($nombre, $apellido)){
+        for ( $pos=0; $pos < strlen($nombre); $pos ++ ) {
+            $byte = substr($nombre, $pos);
+            if(ord($byte)>=97 and ord($byte)<=122){
+                echo "correcto<br>";
+            } else {
+                echo "incorrecto<br>";
+            }
+        }
+    } else {
+        echo "El nombre no comienza con una letra mayúscula.<br>";
+    }
 }
 
-validarLetra($nombre,$apellido);*/
+validarLetra($nombre,$apellido);
 
-//function validarDireccion($direccion)
+function validarDireccion($direccion){
+
+}
 
 function validarEmail($email, $emailconfirmacion){
     if($email == $emailconfirmacion){
-        echo "correcto";
+        echo "correcto<br>";
     } else{
-        echo "incorrecto";
+        echo "incorrecto<br>";
     }    
 }
 
 validarEmail($email, $emailconfirmacion);
-
-
 ?>
