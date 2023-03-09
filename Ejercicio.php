e743a16185c785f3e2c7a21ff1215b7752bb0f70
Alejandro Bermúdez Alba <Alejandrobermudezalba@gmail.com>
2023-03-09 12:57:27 +0100

    actualizar

--- Ejercicio.php
+++ Ejercicio.php
@@ -3,13 +3,33 @@ $nombre = "Pepe";
 $apellido = "Hernández";
 $direccion = "Bajada de San Juan 12";
 $email = "pepehernandez@gmail.com";
-$emailconfirmacion == $email;
+$emailconfirmacion = $email;
 $dni = "72184616G";
 $contraseña = "arbusto";
 
+/*for ( $pos=0; $pos < strlen($nombre); $pos ++ ) {
+    $byte = substr($nombre, $pos); 
+    echo "El byte " . $pos . " de $nombre tiene el valor " . ord($byte) . "<br>" ;
+   }
+function validarMayus($nombre,$apellido){
+if(ord($nombre[0])>=65 and ord($nombre[0])<=90){
+    return true;
+    else {
+    return false;
+    }
+}
+}
+function validarLetra($nombre,$apellido){
+    for ( $pos=0; $pos < strlen($nombre); $pos ++ ) {
+        $byte = substr($nombre, $pos);
+        if(ord($byte)>=97 and ord($byte)<=122)
+        echo "correcto";
+        else 
+        echo "incorrecto";
+}
+}
 
-
-
+validarLetra($nombre,$apellido);*/
 
 
 ?>
