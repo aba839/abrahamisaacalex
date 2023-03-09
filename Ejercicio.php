96c30a4274c77202c6e8136e64be1d02e8b7be13
Antonio Jimenez <antonioisaacjimenez12@gmail.com>
2023-03-09 12:06:27 +0100

    subida

---
+++ Ejercicio.php
@@ -0,0 +1,15 @@
+<?php
+$nombre = "Pepe";
+$apellido = "Hernández";
+$direccion = "Bajada de San Juan 12";
+$email = "pepehernandez@gmail.com";
+$emailconfirmacion == $email;
+$dni = "72184616G";
+$contraseña = "arbusto";
+
+
+
+
+
+
+?>
